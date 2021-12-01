6c3f7422e3e7ec9818d0ab812c3d35c845d4b9f4
3kxhoDark <jmcumbrera@iesrafaelalberti.es>
2021-12-01 18:08:20 +0100

    Initial commit: Algoritmo de ordenación de la burbuja en Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main() {
+    /**
+     * Algoritmo de ordenación de la burbuja implementado en Kotlin
+     */
+    println("Introduzca la lista de números que desee ordenar (sólo números enteros y separados por comas): ")
+    val input = readLine()
+    val splitList = input?.split(",", ignoreCase = true, limit = 0)
+    val numList: MutableList<Int> = mutableListOf()
+
+    splitList?.forEach { numList.add(it.toInt()) }
+
+    var cambio = true
+
+    while(cambio) {
+        cambio = false
+
+        for(i in 0 until numList.size-1){
+            if(numList[i] > numList[i+1]){
+                val temp = numList[i]
+                numList[i] = numList[i+1]
+                numList[i + 1] = temp
+
+                cambio = true
+            }
+        }
+    }
+
+    val numListReversed: MutableList<Int> = numList.asReversed()
+
+    println("Aquí tenemos la lista ordenada de menor a mayor: $numList")
+    println("Aquí tenemos la lista ordenada de mayor a menor: $numListReversed.")
+}
